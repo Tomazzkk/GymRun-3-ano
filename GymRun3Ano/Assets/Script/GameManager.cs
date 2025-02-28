7e65da6fb50bc2a45c8bdb110160e759df43e12d
Tomazzkk <lucasttavares21@gmail.com>
2025-02-28 09:14:26 -0300

    Spawna um mapa e destroi o outro

--- GymRun3Ano/Assets/Script/GameManager.cs
+++ GymRun3Ano/Assets/Script/GameManager.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject mapSegmentPrefab;  // Prefab da parte do mapa
+    public Transform player;             // Referência ao jogador
+    public int maxSegments = 3;          // Quantidade máxima de segmentos ativos
+    public float segmentWidth = 20f;     // Largura de cada segmento
+    public float offsetPos;
+
+    private List<GameObject> activeSegments = new List<GameObject>();
+    private Vector3 nextSpawnPosition;   // Próxima posição para spawnar um segmento
+
     void Start()
     {
-        
+        // Inicia criando os primeiros segmentos
+        for (int i = 0; i < maxSegments; i++)
+        {
+            SpawnSegment();
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Se o jogador ultrapassar a posição do último segmento criado
+        if (player.position.x > nextSpawnPosition.x - (maxSegments * segmentWidth))
+        {
+            SpawnSegment();  // Cria um novo segmento na frente
+           // Remove o segmento mais antigo
+        }
     }
+
+    void SpawnSegment()
+    {
+        GameObject newSegment = Instantiate(mapSegmentPrefab, nextSpawnPosition, Quaternion.identity);
+        activeSegments.Add(newSegment);
+        nextSpawnPosition.x += segmentWidth + offsetPos; // Atualiza posição para o próximo segmento
+        RemoveOldSegment();
+    }
+
+    void RemoveOldSegment()
+    {
+        if (activeSegments.Count > maxSegments)
+        {
+            GameObject oldSegment = activeSegments[0];
+            activeSegments.RemoveAt(0);
+            Destroy(oldSegment);
+        }
+    }
+
 }
