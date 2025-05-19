fa1921d20f3be740ca011b0c33dda828dcce7ac0
Miguelfv07 <fagundesmiguel60@gmail.com>
2025-05-19 11:10:03 -0300

    Menu

--- GymRun3Ano/Assets/Script/MenuScript.cs
+++ GymRun3Ano/Assets/Script/MenuScript.cs
@@ -5,9 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public GameObject creditosPanel;
+    public GameObject panelMenu;
     public void PlayButton()
     {
         SceneManager.LoadScene("SampleScene");
         Time.timeScale = 1.0f;
     }
+
+    public void EntraCreditos()
+    {
+        creditosPanel.SetActive(true);
+        panelMenu.SetActive(false);
+    }
+    
+    public void VoltaCreditos()
+    {
+        creditosPanel.SetActive(false);
+        panelMenu.SetActive(true);
+    }
+
 }
